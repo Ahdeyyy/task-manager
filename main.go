ee890b94f3bb598d16dbbab1523501d956b76cee
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-06-17 14:14:29 +0100

    refactor: Restructure routes to use new app constructor and dependencies

--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"task-manager/routes"
+	"task-manager/store"
+	"time"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	app := fiber.New()
-	app.Static("/", "./static")
-	app.Get("/", routes.HomeRoute)
-	app.Get("/name/:name?", routes.HomeRoute)
-	app.Get("/clicked", routes.Clicked)
 
-	app.Listen(":3000")
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	viper.ReadInConfig()
+	env_db := viper.GetString("DB_CONNECTION_URI")
+	port := viper.GetString("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	connectionUri := "postgres://postgres:password@localhost:5432/?sslmode=disable"
+	if env_db != "" {
+		connectionUri = env_db
+		log.Println(connectionUri)
+	}
+
+	s, err := store.CreateStore(connectionUri)
+
+	if err != nil {
+		panic(err)
+	}
+	defer s.Db.Close()
+	app := routes.NewApp(s, connectionUri)
+
+	app.Use(logger.New())
+	app.Get("/metrics", monitor.New())
+	app.Use(limiter.New(limiter.Config{
+		Max:               20,
+		Expiration:        30 * time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	}))
+
+	app.Listen(fmt.Sprintf(":%s", port))
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,19 +1,49 @@
 package routes
 
 import (
-	"task-manager/views"
+	"database/sql"
+	"task-manager/store"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/gofiber/storage/postgres/v3"
 )
 
-func HomeRoute(c *fiber.Ctx) error {
-	name := c.Params("name")
-	if name == "" {
-		name = "World"
-	}
-	return views.Render(c, views.Home(name))
+type Routes struct {
+	db      *sql.DB
+	storage *postgres.Storage
+	store   *session.Store
 }
 
-func Clicked(c *fiber.Ctx) error {
-	return views.Render(c, views.Clicked())
+func NewApp(s *store.Store, connectionUri string) *fiber.App {
+	// TODO: Pass the database into the app so it can be close properly
+	app := fiber.New()
+
+	storage := postgres.New(postgres.Config{
+		Table:         "session_store",
+		Reset:         false,
+		GCInterval:    3 * time.Second,
+		ConnectionURI: connectionUri,
+	})
+	session_store := session.New(session.Config{
+		Storage:    storage,
+		Expiration: 1 * time.Hour,
+		KeyLookup:  "cookie:myapp_session",
+	})
+
+	routes := Routes{
+		db:      s.Db,
+		storage: storage,
+		store:   session_store,
+	}
+
+	app.Static("/static/", "./static")
+	app.Get("/", routes.HomeRoute)
+	app.Get("/signup", routes.SignupRoute)
+	app.Post("/users/create", routes.CreateNewUser)
+	app.Post("/login", routes.LoginRoute)
+	app.Get("/login", routes.LoginRoute)
+
+	return app
 }
